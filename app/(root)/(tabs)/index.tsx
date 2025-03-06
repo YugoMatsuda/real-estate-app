31615ce1c36e7fbbcea9965b83890c635606337c
Yugo Matsuda <yugo1997zazen@gmail.com>
2025-03-05 16:05:53 -0800

    Display Tab

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,11 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-       <Text className="font-bold text-lg my-10 font-rubik text-3xl">Welcome to Restate</Text>
-      <Link href="/sign-in"> Sign in</Link>
-      <Link href="/explore"> Explore</Link>
-      <Link href="/profile"> Profile</Link>
-      <Link href="/properties/1"> Property 1</Link>
+      <Text className="font-bold text-lg my-10 font-rubik text-3xl">
+        Welcome to Restate
+      </Text>
     </View>
   );
 }
