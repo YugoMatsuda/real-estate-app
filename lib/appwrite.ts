780dc305c5acc0edd45806061c2445ece6e13cd6
Yugo Matsuda <yugo1997zazen@gmail.com>
2025-03-08 20:03:58 -0800

    Set up DB

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -15,6 +15,13 @@ export const config = {
   platform: "com.ugo.restate",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databasesId: process.env.EXPO_PUBLIC_APPWRITE_DATABASES_ID,
+  galleriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+  propertiesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 };
 
 export const client = new Client();
